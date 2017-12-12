7424a5226d1a9c63140de4626fad1fa81b365dfd
Eric <zxinmiao@126.com>
2017-12-13 02:48:09 +0800

    update lastest code
    update lastest code

--- amazing-money-funds-papi/src/main/java/money/funds/controller/MoneyFundsController.java
+++ amazing-money-funds-papi/src/main/java/money/funds/controller/MoneyFundsController.java
@@ -19,7 +19,7 @@ import money.funds.service.MoneyFundsService;
  */
 @RestController
 @CrossOrigin
-@RequestMapping({"/moeney/funds/market", "/api/v1.1"}) // Standard
+@RequestMapping({"/api/money/funds/market", "/api/v1.1"}) // Standard
 public class MoneyFundsController {
     @Autowired
     private MoneyFundsService moneyFundsService;
